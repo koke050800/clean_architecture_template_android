94df2a1fd56562fa8c52f15a038f3658f7a30f28
Jorge Saucedo <67980621+koke050800@users.noreply.github.com>
2026-02-18 14:33:31 -0600

    ✏️ Fix(theme): Name in CamelCase

--- app/src/main/java/com/example/clean_architecture_android_template/MainActivity.kt
+++ app/src/main/java/com/example/clean_architecture_android_template/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.clean_architecture_android_template.ui.theme.Clean_architecture_android_templateTheme
+import com.example.clean_architecture_android_template.ui.theme.CleanArchitectureAndroidTemplateTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Clean_architecture_android_templateTheme {
+            CleanArchitectureAndroidTemplateTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Clean_architecture_android_templateTheme {
+    CleanArchitectureAndroidTemplateTheme {
         Greeting("Android")
     }
 }
--- app/src/main/java/com/example/clean_architecture_android_template/ui/theme/Theme.kt
+++ app/src/main/java/com/example/clean_architecture_android_template/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.clean_architecture_android_template.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -34,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Clean_architecture_android_templateTheme(
+fun CleanArchitectureAndroidTemplateTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
